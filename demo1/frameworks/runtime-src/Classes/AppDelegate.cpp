wantnon/_3DToolKit2-for-cocos2dx-3x

commit ecb137cc0c5b7b1a5264f436263554b65c5be436

fix bug: cube selection demo not work well on device which screen size is not 1136*640

demo1/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,8 +52,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+    auto size = glview->getVisibleSize();
 	if(!glview) {
-        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,1136,640));//Rect(0,0,900,640));
+        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
 
@@ -94,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 //    ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 //	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 //	ScriptingCore::getInstance()->runScript("main.js");
-    glview->setDesignResolutionSize(1136, 640,/* kResolutionNoBorder*/kResolutionShowAll);
+    glview->setDesignResolutionSize(size.width , size.height,/* kResolutionNoBorder*/kResolutionShowAll);
 
     director->runWithScene(HelloWorld::scene());
     return true;

demo2/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,8 +52,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+    auto size = glview->getVisibleSize();
 	if(!glview) {
-        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,1136,640));//Rect(0,0,900,640));
+        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
 
@@ -94,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 //    ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 //	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 //	ScriptingCore::getInstance()->runScript("main.js");
-    glview->setDesignResolutionSize(1136, 640,/* kResolutionNoBorder*/kResolutionShowAll);
+    glview->setDesignResolutionSize(size.width , size.height,/* kResolutionNoBorder*/kResolutionShowAll);
 
     director->runWithScene(HelloWorld::scene());
     return true;

demo3/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,8 +52,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+    auto size = glview->getVisibleSize();
 	if(!glview) {
-        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,1136,640));//Rect(0,0,900,640));
+        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
 
@@ -94,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 //    ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 //	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 //	ScriptingCore::getInstance()->runScript("main.js");
-    glview->setDesignResolutionSize(1136, 640,/* kResolutionNoBorder*/kResolutionShowAll);
+    glview->setDesignResolutionSize(size.width , size.height,/* kResolutionNoBorder*/kResolutionShowAll);
 
     director->runWithScene(HelloWorld::scene());
     return true;

demo4/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,8 +52,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+    auto size = glview->getVisibleSize();
 	if(!glview) {
-        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,1136,640));//Rect(0,0,900,640));
+        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
 
@@ -94,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 //    ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 //	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 //	ScriptingCore::getInstance()->runScript("main.js");
-    glview->setDesignResolutionSize(1136, 640,/* kResolutionNoBorder*/kResolutionShowAll);
+    glview->setDesignResolutionSize(size.width , size.height,/* kResolutionNoBorder*/kResolutionShowAll);
 
     director->runWithScene(HelloWorld::scene());
     return true;

demo5/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,8 +52,9 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+    auto size = glview->getVisibleSize();
 	if(!glview) {
-        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,1136,640));//Rect(0,0,900,640));
+        glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
 
@@ -94,7 +95,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 //    ScriptEngineProtocol *engine = ScriptingCore::getInstance();
 //	ScriptEngineManager::getInstance()->setScriptEngine(engine);
 //	ScriptingCore::getInstance()->runScript("main.js");
-    glview->setDesignResolutionSize(1136, 640,/* kResolutionNoBorder*/kResolutionShowAll);
+    glview->setDesignResolutionSize(size.width , size.height,/* kResolutionNoBorder*/kResolutionShowAll);
 
     director->runWithScene(HelloWorld::scene());
     return true;

commit 44abb83a16579df803938ff7eb3a5cc381e61381

fix bug: fix bug introduced by ecb137c

demo1/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,7 +52,11 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32||CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto size = CCSize(1136,640);
+#elif
     auto size = glview->getVisibleSize();
+#endif
 	if(!glview) {
         glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);

demo2/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,7 +52,11 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32||CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto size = CCSize(1136,640);
+#elif
     auto size = glview->getVisibleSize();
+#endif
 	if(!glview) {
         glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);

demo3/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,7 +52,11 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32||CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto size = CCSize(1136,640);
+#elif
     auto size = glview->getVisibleSize();
+#endif
 	if(!glview) {
         glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);

demo4/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,7 +52,11 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32||CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto size = CCSize(1136,640);
+#elif
     auto size = glview->getVisibleSize();
+#endif
 	if(!glview) {
         glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);

demo5/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -52,7 +52,12 @@ bool AppDelegate::applicationDidFinishLaunching()
     // initialize director
     auto director = Director::getInstance();
 	auto glview = director->getOpenGLView();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32||CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+    auto size = CCSize(1136,640);
+#else
     auto size = glview->getVisibleSize();
+#endif
+    
 	if(!glview) {
         glview = cocos2d::GLViewImpl::createWithRect("HelloWorld", Rect(0,0,size.width,size.height));//Rect(0,0,900,640));
 		director->setOpenGLView(glview);